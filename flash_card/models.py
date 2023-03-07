2ae3572d7e66ea000e8d9913852cc4391f409c3d
MohamadrezaPiri <piry.mreza@gmail.com>
2023-03-07 17:36:49 +0330

    Add __stt__ method to FlashCard model

--- flash_card/models.py
+++ flash_card/models.py
@@ -8,3 +8,6 @@ class FlashCard(models.Model):
     answer = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
