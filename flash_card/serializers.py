08a9e0e86b0ec9d1f1cb24ab11986130c5d19dcc
MohamadrezaPiri <piry.mreza@gmail.com>
2023-03-08 13:29:38 +0330

    Add CreateFlashCardSerializer for creating a flash card

--- flash_card/serializers.py
+++ flash_card/serializers.py
@@ -1,7 +1,15 @@
 from djoser.serializers import UserCreateSerializer
+from rest_framework.serializers import ModelSerializer
+from .models import FlashCard
 
 
 class CreateUserProfileSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         fields = ['id', 'username', 'first_name',
                   'last_name', 'email', 'password']
+
+
+class CreateFlashCardSerializer(ModelSerializer):
+    class Meta:
+        model = FlashCard
+        fields = '__all__'
