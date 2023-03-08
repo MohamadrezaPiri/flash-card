9718237288c24ede61fb3519b347f1625d690e0d
MohamadrezaPiri <piry.mreza@gmail.com>
2023-03-08 13:38:45 +0330

    Add FlashCartViewSet

--- flash_card/views.py
+++ flash_card/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .models import FlashCard
+from .serializers import CreateFlashCardSerializer
 # Create your views here.
+
+
+class FlashCardViewSet(ModelViewSet):
+    queryset = FlashCard.objects.all()
+    serializer_class = CreateFlashCardSerializer
