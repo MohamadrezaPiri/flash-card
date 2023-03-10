bf0d3fafe7ebe840390ce86ada2da5dbbdfc60bb
MohamadrezaPiri <piry.mreza@gmail.com>
2023-03-10 11:15:02 +0330

    Add permission class to FlashCartViewSet

--- flash_card/views.py
+++ flash_card/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from .models import FlashCard
 from .serializers import CreateFlashCardSerializer
 # Create your views here.
@@ -7,3 +8,4 @@ from .serializers import CreateFlashCardSerializer
 class FlashCardViewSet(ModelViewSet):
     queryset = FlashCard.objects.all()
     serializer_class = CreateFlashCardSerializer
+    permission_classes = (IsAuthenticated,)
