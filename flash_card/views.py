fb2c9644ad2c714507c336ec9a11a578ca99ee93
MohamadrezaPiri <piry.mreza@gmail.com>
2023-03-10 12:55:21 +0330

    Change FlashCardViewSet permission to IsAuthenticatedOrReadOnly

--- flash_card/views.py
+++ flash_card/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import FlashCard
 from .serializers import CreateFlashCardSerializer
 # Create your views here.
@@ -8,7 +8,7 @@ from .serializers import CreateFlashCardSerializer
 class FlashCardViewSet(ModelViewSet):
     queryset = FlashCard.objects.all()
     serializer_class = CreateFlashCardSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_serializer_context(self):
         return {'user_id': self.request.user.id}
