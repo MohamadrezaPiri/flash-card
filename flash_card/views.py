0a0a41ea0d28d72f393865455aa3be9e99822ee1
MohamadrezaPiri <piry.mreza@gmail.com>
2023-03-13 14:27:34 +0330

    Add ordering fields to FlashCardViewSet

--- flash_card/views.py
+++ flash_card/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .permissions import IsOwnerOrReadOnly
 from .models import FlashCard
@@ -11,8 +11,9 @@ class FlashCardViewSet(ModelViewSet):
     queryset = FlashCard.objects.all()
     serializer_class = CreateFlashCardSerializer
     permission_classes = (IsOwnerOrReadOnly,)
-    filter_backends = (SearchFilter,)
+    filter_backends = (SearchFilter, OrderingFilter)
     search_fields = ('question',)
+    ordering_fields = ('created_at',)
 
     def get_serializer_context(self):
         return {'user_id': self.request.user.id}
