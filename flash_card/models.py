9b687b99d3d7017dd4f1d830204acb1d708d9a06
MohamadrezaPiri <piry.mreza@gmail.com>
2023-03-13 14:44:46 +0330

    Add category model

--- flash_card/models.py
+++ flash_card/models.py
@@ -6,6 +6,10 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
 class FlashCard(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.TextField()
