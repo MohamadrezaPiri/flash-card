075bb816419d2019492658e8723fe632da6e8601
MohamadrezaPiri <piry.mreza@gmail.com>
2023-03-13 14:14:57 +0330

    Add search field to FlashCardViewSet

--- flash_card/views.py
+++ flash_card/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from .permissions import IsOwnerOrReadOnly
 from .models import FlashCard
 from .serializers import CreateFlashCardSerializer
@@ -9,6 +11,8 @@ class FlashCardViewSet(ModelViewSet):
     queryset = FlashCard.objects.all()
     serializer_class = CreateFlashCardSerializer
     permission_classes = (IsOwnerOrReadOnly,)
+    filter_backends = (SearchFilter,)
+    search_fields = ('question',)
 
     def get_serializer_context(self):
         return {'user_id': self.request.user.id}
