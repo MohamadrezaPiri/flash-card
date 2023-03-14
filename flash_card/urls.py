62dff4aa91aabdd783a42aa75ce7db67bab4ac77
MohamadrezaPiri <piry.mreza@gmail.com>
2023-03-14 11:21:34 +0330

    Add category router in urls.py file

--- flash_card/urls.py
+++ flash_card/urls.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
-from .views import FlashCardViewSet
+from .views import FlashCardViewSet, CategoryViewSet
 
 router = routers.SimpleRouter()
 router.register('cards', FlashCardViewSet)
+router.register('categories', CategoryViewSet)
 
 urlpatterns = router.urls
