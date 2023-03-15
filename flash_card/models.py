2de0baa1af3f13adc97be554aba8e6f2787c07d4
MohamadrezaPiri <piry.mreza@gmail.com>
2023-03-15 13:45:11 +0330

    Define __str__ method in Category model

--- flash_card/models.py
+++ flash_card/models.py
@@ -9,6 +9,9 @@ User = get_user_model()
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class FlashCard(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
