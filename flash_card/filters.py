b239f3e3645e0af4efc8052bb2503f08d69da4b9
MohamadrezaPiri <piry.mreza@gmail.com>
2023-03-16 13:13:49 +0330

    Create FlashCardFilters at filters.py file

---
+++ flash_card/filters.py
@@ -0,0 +1,10 @@
+from django_filters.rest_framework import FilterSet
+from .models import FlashCard
+
+
+class FlashCardFilters(FilterSet):
+    class Meta:
+        model = FlashCard
+        fields = {
+            'category': ['exact']
+        }
