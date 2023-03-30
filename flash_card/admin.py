3065883a0b130641adab558ff4731febcf9932f1
MohamadrezaPiri <piry.mreza@gmail.com>
2023-03-30 14:38:09 +0430

    Add list_select_related field to FlashCardAdmin

--- flash_card/admin.py
+++ flash_card/admin.py
@@ -12,6 +12,7 @@ class FlashCardAdmin(admin.ModelAdmin):
     list_per_page = 10
     autocomplete_fields = ('user', 'category',)
     search_fields = ('question',)
+    list_select_related=('category',)
 
 
 @admin.register(Category)
