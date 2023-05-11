a55f7f882554961f72eb613808efa23da94812c0
MohamadrezaPiri <piry.mreza@gmail.com>
2023-05-11 12:28:35 +0430

    Create CardsCountFilter in filters.py file

--- flash_card/filters.py
+++ flash_card/filters.py
@@ -1,4 +1,6 @@
 from django_filters.rest_framework import FilterSet
+from django.db.models import Count
+from django.contrib import admin
 from .models import FlashCard
 
 
@@ -8,3 +10,21 @@ class FlashCardFilters(FilterSet):
         fields = {
             'category_id': ['exact']
         }
+
+
+class CardsCountFilter(admin.SimpleListFilter):
+    title = 'Cards count'
+    parameter_name = 'FlashCard_set'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<1','Without Card'),
+            ('0<','With Card')
+        ]
+    
+    def queryset(self, request, queryset):
+        annotated_value = queryset.annotate(cards_count = Count('flashcard'))
+        if self.value() == '<1':
+            return annotated_value.filter(cards_count__lt=1)
+        elif self.value() == '0<':
+            return annotated_value.filter(cards_count__gt=0)
