ec9ee0f9b7f4eebc5251dddccd3aa9a9ec46c427
MohamadrezaPiri <piry.mreza@gmail.com>
2023-05-11 12:11:38 +0430

    Add user__username to search_fields in FlashCardAdmin

--- flash_card/admin.py
+++ flash_card/admin.py
@@ -14,7 +14,7 @@ class FlashCardAdmin(admin.ModelAdmin):
     list_editable = ('category',)
     list_per_page = 10
     autocomplete_fields = ('user', 'category',)
-    search_fields = ('question',)
+    search_fields = ('question','user__username')
     list_select_related=('category',)
 
 
